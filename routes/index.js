ec7c2e88589341399c9b25c3a4bf91f7435c99fd
johnshawstudent <john.shaw@mygeorgian.ca>
2016-02-13 12:24:14 -0500

    Site structure
    set up all the site pages with header and footer partials

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { 
+  	title: 'About Me' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { 
+  	title: 'Projects' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { 
+  	title: 'Services' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { 
+  	title: 'Contact Me' });
 });
 
 module.exports = router;
